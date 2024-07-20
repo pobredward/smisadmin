2e6879745f5e97d08e5539ce4cd3ff13329caa76
Edward Shin <pobredward@gmail.com>
2024-07-20 00:06:37 +0000

    Update students.js

--- pages/api/students.js
+++ pages/api/students.js
@@ -1,7 +1,7 @@
 import { google } from "googleapis";
 import NodeCache from "node-cache";
 
-const cache = new NodeCache({ stdTTL: 300 }); // 5분 TTL
+const cache = new NodeCache({ stdTTL: 3600 }); // 1시간 TTL
 
 const sheets = google.sheets("v4");
 
@@ -11,12 +11,13 @@ async function getAuthClient() {
       type: process.env.GOOGLE_TYPE,
       project_id: process.env.GOOGLE_PROJECT_ID,
       private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // 줄바꿈 문자 처리
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // 줄바꿈 문자 처리
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
       client_id: process.env.GOOGLE_CLIENT_ID,
       auth_uri: process.env.GOOGLE_AUTH_URI,
       token_uri: process.env.GOOGLE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+      auth_provider_x509_cert_url:
+        process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
       client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
       universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
     },
